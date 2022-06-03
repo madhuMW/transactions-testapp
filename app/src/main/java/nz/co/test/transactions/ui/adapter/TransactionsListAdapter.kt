158f3d2417c479e8eaed290228555b847bbf2b78
madhubha <madhubhamw@gmail.com>
2022-06-03 23:53:42 +1200

    added TransactionsListAdapter

---
+++ app/src/main/java/nz/co/test/transactions/ui/adapter/TransactionsListAdapter.kt
@@ -0,0 +1,73 @@
+package nz.co.test.transactions.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import nz.co.test.transactions.data.services.Transaction
+import nz.co.test.transactions.databinding.ItemTransactionBinding
+
+class TransactionsListAdapter(
+    private val adapterListener: TransactionsListAdapterListener,
+    private val context: Context
+) :
+    ListAdapter<Transaction, TransactionsListAdapter.ViewHolder>(
+        DiffCallback
+    ) {
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Transaction>() {
+            override fun areItemsTheSame(oldItem: Transaction, newItem: Transaction): Boolean =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: Transaction, newItem: Transaction): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemTransactionBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.apply {
+            holder.bind(data!!)
+        }
+    }
+
+    inner class ViewHolder(val binding: ItemTransactionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val item = getItem(position)
+                    if (item != null) {
+                        adapterListener.onItemTap(item)
+                    }
+                }
+            }
+        }
+
+        fun bind(transaction: Transaction) {
+            binding.apply {
+                id.text = transaction.id.toString()
+                transactionDate.text = transaction.transactionDate.toString()
+            }
+        }
+    }
+
+    interface TransactionsListAdapterListener {
+        fun onItemTap(transaction: Transaction)
+    }
+}
--- app/src/main/java/nz/co/test/transactions/ui/main/TransactionListFragment.kt
+++ app/src/main/java/nz/co/test/transactions/ui/main/TransactionListFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import nz.co.test.transactions.R
 import nz.co.test.transactions.databinding.FragmentTransactionListBinding
+import nz.co.test.transactions.ui.adapter.TransactionsListAdapter
 
-class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
+class TransactionListFragment : Fragment(R.layout.fragment_transaction_list),
+    TransactionsListAdapter.TransactionsListAdapterListener {
     private var _binding: FragmentTransactionListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var transactionsListAdapter: TransactionsListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +23,15 @@ class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentTransactionListBinding.inflate(inflater, container, false)
+        transactionsListAdapter = TransactionsListAdapter(this, requireActivity())
+
+        binding.apply {
+            transactionsRecycler.layoutManager = LinearLayoutManager(requireActivity())
+            transactionsRecycler.adapter = transactionsListAdapter
+            transactionsRecycler.adapter?.stateRestorationPolicy =
+                RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        }
+
         return binding.root
     }
 
@@ -25,5 +39,4 @@ class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
         super.onDestroy()
         _binding = null
     }
-
 }
