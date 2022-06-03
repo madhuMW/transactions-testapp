d5672fb3ebeed3a48f5a8f6b9f2571b73842ec40
madhubha <madhubhamw@gmail.com>
2022-06-04 01:05:38 +1200

    observe LiveData

--- app/src/main/java/nz/co/test/transactions/ui/main/TransactionListFragment.kt
+++ app/src/main/java/nz/co/test/transactions/ui/main/TransactionListFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 import nz.co.test.transactions.R
 import nz.co.test.transactions.data.services.Transaction
 import nz.co.test.transactions.databinding.FragmentTransactionListBinding
@@ -18,6 +22,7 @@ class TransactionListFragment : Fragment(R.layout.fragment_transaction_list),
     private var _binding: FragmentTransactionListBinding? = null
     private val binding get() = _binding!!
     private lateinit var transactionsListAdapter: TransactionsListAdapter
+    private val transViewModel: TransactionListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +39,22 @@ class TransactionListFragment : Fragment(R.layout.fragment_transaction_list),
                 RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
         }
 
+        transViewModel.transactionsListLiveData.observe(
+            viewLifecycleOwner,
+            Observer {
+                transactionsListAdapter.submitList(it)
+            }
+        )
+
+        transViewModel.errorLiveData.observe(
+            viewLifecycleOwner,
+            Observer {
+            }
+        )
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            transViewModel.getTransactionList()
+        }
         return binding.root
     }
 
