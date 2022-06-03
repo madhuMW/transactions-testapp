ea2f3b247800c3a85fcc6856822bb2ec1fcdd110
madhubha <madhubhamw@gmail.com>
2022-06-04 00:47:59 +1200

    added TransactionListViewModel

---
+++ app/src/main/java/nz/co/test/transactions/di/coroutine/CoroutineModule.kt
@@ -0,0 +1,19 @@
+package nz.co.test.transactions.di.coroutine
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CoroutineModule {
+    @Provides
+    @Singleton
+    fun provideDispatcherDefault(): CoroutineDispatcher {
+        return Dispatchers.Default
+    }
+}
---
+++ app/src/main/java/nz/co/test/transactions/ui/main/TransactionListViewModel.kt
@@ -0,0 +1,34 @@
+package nz.co.test.transactions.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import nz.co.test.transactions.data.services.Transaction
+import nz.co.test.transactions.interactors.GetTransactions
+import nz.co.test.transactions.utils.ResponseResult
+import javax.inject.Inject
+
+@HiltViewModel
+class TransactionListViewModel @Inject constructor(
+    private val coroutineDispatcher: CoroutineDispatcher,
+    private val getTransactions: GetTransactions
+) :
+    ViewModel() {
+    private val transactionsListMutableLiveData = MutableLiveData<List<Transaction>>()
+    val transactionsListLiveData: LiveData<List<Transaction>> get() = transactionsListMutableLiveData
+    private val errorMutableLiveData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String> get() = errorMutableLiveData
+
+    fun getTransactionList() {
+        viewModelScope.launch(coroutineDispatcher) {
+            when (val result = getTransactions()) {
+                is ResponseResult.Success -> transactionsListMutableLiveData.postValue(result.data)
+                is ResponseResult.Error -> errorMutableLiveData.postValue(result.exception.message)
+            }
+        }
+    }
+}
