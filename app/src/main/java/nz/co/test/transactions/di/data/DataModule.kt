e35ea6232883f7bc752f08421931fa16828a43ee
madhubha <madhubhamw@gmail.com>
2022-06-03 22:31:54 +1200

    modified DataModule to provide Repository

--- app/src/main/java/nz/co/test/transactions/di/data/DataModule.kt
+++ app/src/main/java/nz/co/test/transactions/di/data/DataModule.kt
@@ -1,14 +1,19 @@
 package nz.co.test.transactions.di.data
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import nz.co.test.transactions.data.mapper.IMapper
+import nz.co.test.transactions.data.repository.ITransactionRepository
 import nz.co.test.transactions.data.services.Transaction
+import nz.co.test.transactions.data.services.TransactionsService
 import nz.co.test.transactions.data.services.response.TransactionDTO
 import nz.co.test.transactions.framework.Mapper
+import nz.co.test.transactions.framework.TransactionRepository
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
@@ -17,5 +22,15 @@ class DataModule {
     @ActivityRetainedScoped
     fun provideMapper(): IMapper<List<TransactionDTO>, List<Transaction>> {
         return Mapper()
+    }
+
+    @Provides
+    @ActivityRetainedScoped
+    fun provideRepository(
+        transactionsService: TransactionsService,
+        mapper: IMapper<List<TransactionDTO>, List<Transaction>>,
+        @ApplicationContext context: Context
+    ): ITransactionRepository {
+        return TransactionRepository(transactionsService, mapper, context)
     }
 }
